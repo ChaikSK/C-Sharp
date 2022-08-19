ec28b03ea6ebe1b677fb9d921212a3e6d10b38fb
CSK <71926801+ChaikSK@users.noreply.github.com>
2022-08-19 23:12:44 +0800

    Add files via upload

---
+++ C# Tutorial/Duck/18_ Building an Exponent Method/18_ Building an Exponent Method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18__Building_an_Exponent_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exponent Function can pass in two numbers (base number and power number) and take base number to power number
+            // Eg, 2,3 -> 2^3 or 4,7 -> 4^7
+
+            Console.WriteLine(GetPow(3, 2));
+
+            Console.ReadLine();
+
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result *= baseNum;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C# Tutorial/Duck/20_ Comments/20_ Comments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20__Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Comments");
+
+            Console.ReadLine();
+        }
+    }
+}
