9fca87174d9467bca4bc05c76628846c3f71fb30
CSK <71926801+ChaikSK@users.noreply.github.com>
2022-08-21 00:00:53 +0800

    Add files via upload

---
+++ 21_ Classes N Objects/21_ Classes N Objects/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21__Classes_N_Objects
+{
+    class Book  // create a complex datatype - composed all other dataype
+    {
+        // class attributes
+        public string title;
+        public string author;
+        public int pages;
+
+
+    }
+}
